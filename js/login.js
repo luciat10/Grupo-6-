80bfd3ca572c0021da71c1c7dffb330506cb4d49
evelynrodriguezpais <evelyn.rodriguezpais225@gmail.com>
2024-08-22 16:19:15 -0300

    validacion login

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function login() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    // Verificar que ambos campos estén completos
+    if (username === "" || password === "") {
+        // Mostrar una alerta si los campos están incompletos
+        alert("Por favor, complete ambos campos.");
+    } else {
+        // Guardar el inicio de sesión en sessionStorage
+        sessionStorage.setItem('isLoggedIn', true);
+        // Almacenar el nombre de usuario en localStorage
+        localStorage.setItem('username', username);
+        // Redirigir a la página index.html
+        window.location.href = "index.html";
+    }
+}
+
+// Asociar la función al evento click del botón de registro
+document.getElementById('registerButton').addEventListener('click', login);
