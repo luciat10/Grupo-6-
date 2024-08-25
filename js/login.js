a575ffa838c4f047ef46cbcbaa6a12def8681c37
evelynrodriguezpais <evelyn.rodriguezpais225@gmail.com>
2024-08-25 18:59:51 -0300

    arreglo de validacion login

--- js/login.js
+++ js/login.js
@@ -1,20 +1,10 @@
-function login() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-
-    // Verificar que ambos campos estén completos
-    if (username === "" || password === "") {
-        // Mostrar una alerta si los campos están incompletos
-        alert("Por favor, complete ambos campos.");
-    } else {
-        // Guardar el inicio de sesión en sessionStorage
-        sessionStorage.setItem('isLoggedIn', true);
-        // Almacenar el nombre de usuario en localStorage
-        localStorage.setItem('username', username);
-        // Redirigir a la página index.html
-        window.location.href = "index.html";
-    }
-}
-
-// Asociar la función al evento click del botón de registro
-document.getElementById('registerButton').addEventListener('click', login);
+document.addEventListener("DOMContentLoaded", (event)=>{
+    let loginBtn = document.getElementById("loginBtn");
+    loginBtn.addEventListener("click", ()=>{
+        let usuario = document.getElementById("usuario").value;
+        let contraseña = document.getElementById("contraseña").value;
+        if (usuario!="" && contraseña!=""){
+            window.location.href = "index.html";
+        };
+    });
+});
