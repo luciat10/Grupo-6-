63aaee69e966dc9b89680aef32dbe9fe75760d72
Victoria Leone <vickyleone@MacBook-Pro-de-Victoria.local>
2024-08-25 21:21:17 -0300

    validacion de login y arreglos login.html

---
+++ js/login.js
@@ -0,0 +1,33 @@
+
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const errorMessage = document.getElementById('error-message');
+
+    if (username.trim() === '' || password.trim() === ''){
+        alert("Por favor, complete ambos campos.")
+    } else {
+        window.location.href = 'index.html';
+    }
+}
+
+document.addEventListener("DomContentLoaded", () => {
+    let login = document.getElementById("login"); 
+    login.addEventListener("click", () => {
+        let usuario = document.getElementById("usuario").value;
+        let contraseña = document.getElementById("contraseña").value;
+          //guardo sesion con localStorage
+          localStorage.setItem("usuario", JSON.stringify(ususario));
+          localStorage.setItem("contraseña", JSON.stringify(contraseña));
+          let ObjUsuario = JSON.parse(localStorage.getItem("usuario"));
+          let ObjContraseña =JSON.parse(localStorage.getItem("contraseña"));
+          if(usuario!="" && contraseña!=""){
+            window.location.href= "index.html";
+          }
+    })
+})
+
+
+
+
