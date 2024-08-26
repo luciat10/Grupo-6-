836b966d34476cd1d1daf619303ca96e992fec35
Victoria Leone <167111980+vleonediaz@users.noreply.github.com>
2024-08-25 21:57:19 -0300

    desafiate

--- js/login.js
+++ js/login.js
@@ -9,3 +9,18 @@ function login() {
     }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    let login = document.getElementById("login"); 
+    login.addEventListener("click", () => {
+        let usuario = document.getElementById("usuario").value;
+        let contraseña = document.getElementById("contraseña").value;
+          //guardo sesion con localStorage
+          localStorage.setItem("usuario", JSON.stringify(usuario));
+          localStorage.setItem("contraseña", JSON.stringify(contraseña));
+          let ObjUsuario = JSON.parse(localStorage.getItem("usuario"));
+          let ObjContraseña =JSON.parse(localStorage.getItem("contraseña"));
+          if(usuario!="" && contraseña!=""){
+            window.location.href= "index.html";
+          }
+    })
+})
